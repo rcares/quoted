4f9cc6fb97c67d6428462d1bf80343adade984ca
Rodrigo Cares <215658+rcares@users.noreply.github.com>
2020-05-13 23:30:38 -0400

    Improve cache usage on spiders

--- quoted/quoted.py
+++ quoted/quoted.py
@@ -10,7 +10,7 @@ from scrapy.crawler import CrawlerProcess
 from rich.console import Console
 from rich.logging import RichHandler
 # spiders
-from quoted.scrapy.spiders import toscrape, brainyquote, goodreads
+from .scrapy.spiders import toscrape, brainyquote, goodreads
 
 # Logging
 logger = logging.getLogger(__name__)
@@ -18,6 +18,9 @@ logger = logging.getLogger(__name__)
 # Paths
 QUOTED_DIR = str(Path.home().joinpath('.quoted'))
 QUOTED_CACHE_DIR = str(Path(QUOTED_DIR).joinpath('cache'))
+QUOTED_CACHE_CONFIG_FILE = str(
+    Path(QUOTED_CACHE_DIR).joinpath('settings.json')
+)
 
 # Buffer
 bytestream = io.BytesIO()
@@ -59,6 +62,7 @@ def do_crawl(spider, cache=0, cache_dir='httpcache'):
         Path(cache_dir).mkdir(parents=True, exist_ok=True)
 
     process = CrawlerProcess(settings={
+        "QUOTED_CACHE_CONFIG_FILE": QUOTED_CACHE_CONFIG_FILE,
         "LOG_ENABLED": False,
         "TELNETCONSOLE_ENABLED": False,
         "HTTPCACHE_ENABLED": False if cache == 0 else True,
--- quoted/scrapy/spiders/toscrape.py
+++ quoted/scrapy/spiders/toscrape.py
@@ -1,17 +1,30 @@
-import scrapy
+from . import QuotedSpider
 
 
-class QuotesSpider(scrapy.Spider):
+class QuotesSpider(QuotedSpider):
     name = 'Quotes to Scrape'
+    base_url = 'http://quotes.toscrape.com'
     start_urls = [
-        'http://quotes.toscrape.com/tag/humor/',
+        '%s/tag/humor/' % base_url,
     ]
+
+    def update_from_cache(self):
+        pass
+
+    def options_to_cache(self):
+        return {}
+
+    def options_from_cache(self, cache_config):
+        pass
 
     def parse(self, response):
         for quote in response.css('div.quote'):
+            tags = quote.css('meta.keywords').attrib['content'].split(',')
             yield {
-                'author': quote.xpath('span/small/text()').get(),
-                'text': quote.css('span.text::text').get(),
+                'author': quote.xpath('span/small/text()').get().strip(', \n\r\t“”'),
+                'text': quote.css('span.text::text').get().strip(', \n\r\t“”'),
+                'url': '%s%s' % (self.base_url, quote.css('span a::attr("href")').get()),
+                'tags': tags
             }
 
         next_page = response.css('li.next a::attr("href")').get()
